cf6bc3327af5b228eaec1db7fa385a24a418f4c5
Nathan Daly <nhdaly@gmail.com>
2018-12-11 14:10:58 -0500

    Allow running with BenchmarkTools.BenchmarkDiff

--- src/runbenchmark.jl
+++ src/runbenchmark.jl
@@ -231,3 +231,13 @@ function _run(b::BenchmarkTools.Benchmark, p::BenchmarkTools.Parameters = b.para
     end
     return res
 end
+
+function PkgBenchmark._run(b::BenchmarkTools.BenchmarkDiff, p::BenchmarkTools.Parameters = b.params;
+                    prog = nothing, verbose::Bool = false, pad = "", hierarchy = [], kwargs...)
+    res = BenchmarkTools.run_result(b, p; kwargs...)[1]
+    if prog != nothing
+        indent = 0
+        ProgressMeter.next!(prog; showvalues = [map(id -> ("  "^(indent += 1) * "[$(id[2])/$(id[3])]", id[1]), hierarchy)...])
+    end
+    return res
+end
